71ee1b49caed452f824c4eee36f0c6fae4d3e3be
umakusumuru <kusumuruuma@gmail.com>
2017-01-10 19:21:40 +0530

    checkn

--- dist/src/main.js
+++ dist/src/main.js
@@ -3,7 +3,9 @@
  * Main App Creation
  */
 
-var editviewapp = angular.module('KaakateeyaEdit', ['ui.router', 'ngAnimate', 'ngSanitize', 'ui.bootstrap', 'ngMaterial', 'ngMdIcons', 'jcs-autoValidate', 'angularPromiseButtons']);
+var editviewapp = angular.module('KaakateeyaEdit', ['ui.router', 'ngAnimate', 'ngSanitize', 'ui.bootstrap',
+    'ngMaterial', 'ngMdIcons', 'jcs-autoValidate'
+]);
 editviewapp.apipath = 'http://183.82.0.58:8010/Api/';
 editviewapp.templateroot = 'editview/';
 
