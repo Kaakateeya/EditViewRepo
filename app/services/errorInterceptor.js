63eaa4a16511603b8b392d8ff1609f2e2b0b8f23
umakusumuru <kusumuruuma@gmail.com>
2017-11-15 13:55:44 +0530

    15_11_2017

--- app/services/errorInterceptor.js
+++ app/services/errorInterceptor.js
@@ -9,6 +9,9 @@
                 $rootScope.loading = true;
                 // }
                 config.headers = config.headers || {};
+                config.headers['Content-Type'] = 'application/json';
+                config.headers['Authorization'] = 'Bearer ' + sessionStorage.getItem('token');
+
                 return config;
             },
             responseError: function(rejection) {
